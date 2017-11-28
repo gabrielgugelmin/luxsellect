e4c78a436b3948152d5492355d06652ad71f5e51
gabrielgugelmin <gaab.fg@gmail.com>
2017-11-27 22:02:23 -0200

    Seção de Info e Instagram feitas

--- assets/js/js.js
+++ assets/js/js.js
@@ -71,6 +71,19 @@ $(function(){
   if ($('.js-grid').length) {
     getProducts();
   }
+
+  if ($('#instafeed').length) {
+
+    var feed = new Instafeed({
+      get: 'user',
+      userId: '4167153856',
+      clientId: '9dd32bbb00284a19b83ebe8dbda91cb6',
+      accessToken: '4167153856.1677ed0.7cfe7712070d4c5da39d9a197a0f4d9a',
+      limit: 12,
+      template: '<li class="Instafeed__item" style="background-image: url({{image}});"><a href="{{link}}"></a></li>'
+    });
+    feed.run();
+  }
 });
 
 
